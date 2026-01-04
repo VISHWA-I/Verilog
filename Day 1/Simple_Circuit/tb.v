6b23e20d7892b56d46a736eda28406992b6d4c8a
VISHWA-I <vishwai11a22122@gmail.com>
2026-01-04 19:18:20 +0530

    Update tb.v

--- Day 1/Simple_Circuit/tb.v
+++ Day 1/Simple_Circuit/tb.v
@@ -1,1 +1,18 @@
+module tb;
+reg a,c,b;
+wire y;
+Gate dut(a,c,b,y);
+initial 
+begin
+    $dumpfile("Gate.vcd");
+    $dumpvars();
+    $monitor("a=%b | c=%b | y=%b",a,c,y);
+
+    a=0;c=0;b=1; #10
+    a=0;c=1; #10
+    a=1;c=0; #10
+    a=1;c=1; #10
+    $finish;
+    end
+    endmodule
 
