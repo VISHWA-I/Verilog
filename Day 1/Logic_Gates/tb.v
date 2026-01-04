d5896c856d79954582d22bcc59e66410c362dad7
VISHWA-I <vishwai11a22122@gmail.com>
2026-01-04 19:06:43 +0530

    Update tb.v

--- Day 1/Logic_Gates/tb.v
+++ Day 1/Logic_Gates/tb.v
@@ -1,1 +1,16 @@
-
+module tb;
+reg a,b;
+wire out1,out2,out3;
+and_gate uut(a,b,out1,out2,out3);
+initial begin
+    $dumpfile("and_gate.vcd");
+    $dumpvars();
+    $monitor("a = %b | b = %b | out1 = %b | out2 = %b | out3 = %b",a,b,out1,out2,out3);
+    a=0;b=0;#10
+    a=0;b=1;#10
+    a=1;b=0;#10
+    a=1;b=1;#10
+    #20
+    $finish;
+end
+endmodule
